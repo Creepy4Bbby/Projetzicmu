1e4e4eecc34bc1f3e9f1c64cabc49cf33ec6c5e2
unknown <dolores.hv3t@gmail.com>
2022-04-14 09:42:28 +0100

    pdf

--- vues/pdf_inscription.php
+++ vues/pdf_inscription.php
@@ -1,100 +1,106 @@
 <?php
 
 // permet d'inclure la bibliothèque fpdf
-require('PDF/fpdf.php');
+// require('PDF/fpdf.php');
 
 
 // instancie un objet de type FPDF qui permet de créer le PDF
-$pdf=new FPDF('L','mm','A4');
+// $pdf=new FPDF('L','mm','A4');
 
 // ajoute une page
-$pdf->AddPage();
+// $pdf->AddPage();
 
 // définit la police courante
-$pdf->SetFont('Helvetica','',16);
+// $pdf->SetFont('Helvetica','',16);
 // $pdf->SetFont('Helvetica','B',16);
 
 Function affPdf($tab){
 
+    require('PDF/fpdf.php');
 
-$pdf->Cell(50,10,utf8_decode('Merci de Faire confiance au Conservatoire ZIQMU '));
-$pdf->Cell(90,10,utf8_decode('Carte du cour n°'));
-$pdf->Cell(100,100, $tab["numero"], 0,1);
-// Affiché image 
-$pdf->Ln(10);
-        // valeur 1 = vers la droite , valeur 2 = vers le bas  valeur 3 = largeur    valeur 4 hauteur                
-$pdf->Image('images/Conservatoire.jpg',130,10, 160, 110);
-$pdf->Ln(100);
 
-// affiche du texte
-    // $pdf->Cell(40,10, $tab["nomAd"], 0,1);
-    // $pdf->Cell(40,10, $tab["prenomAd"], 0,1);
-    // $pdf->Cell(40,10, $tab["date"], 0,1);
-    // $pdf->Cell(40,10, $tab[""], 0,1);
+    $pdf = new FPDF('L','mm','A4');
+    $pdf ->SetLineWidth(1.5);
+    // Titres des colonnes
+    $header = array('Nom ', 'Prénom', 'Jour et heure', 'Professeur', 'Instrument');
+    // Chargement des données
+    //$data = $pdf->LoadData('pays.txt');
+    $pdf->AddPage();
 
-  // end fonction
+    $pdf->SetFont('Courier','BIU',18);
+    $pdf ->Cell(180,10,'ZIK-MU','LTRB', '0', 'C');
+    $pdf-> Ln();
+    $pdf->SetFont('Helvetica','BI',16);
+    $pdf ->Cell(40,15,'Le conservatoire de musique pour monter en competence');
+ 
+
+    $pdf-> Ln();
+    $pdf-> Ln(); 
+    $pdf-> Ln();
+    $pdf-> Ln();
+    $pdf-> Ln();
+    $pdf-> Ln();
+
+    // $pdf->Image('images/conservatoires.jpg',48,35, 110, 60);
+
+    $pdf ->SetLineWidth(0.7);
+    $pdf ->Line(0, 100, 220, 100);
+
+    $pdf->SetFont('Arial','',13);
+    $pdf -> cell(40, 10, 'Voici un recapitulatif de votre inscription:',0, 0, 2);
+
+    $pdf-> Ln();
+    $pdf ->SetLineWidth(0.2);
+    $pdf->SetFont('Times','B',16);
+    $w = array(30, 30, 60, 30, 30);
+     // En-tête
+    for($i=0;$i<count($header);$i++)
+    $pdf->Cell($w[$i],7,utf8_decode($header[$i]),1,0,'C');
+    $pdf->Ln();
+    $pdf->SetFont('Arial','',12);
+     // Données
+     $pdf->Cell($w[0],6,$tab['nomAd'],1,'LR', 'C');
+     $pdf->Cell($w[1],6,$tab['prenomAd'],1,'LR', 'C');
+     $pdf->Cell($w[2],6,$tab['date'],1,'LR', 'C');
+     $pdf->Cell($w[3],6,$tab['nomProf'],1,'LR', 'C');
+     $pdf->Cell($w[4],6,$tab['instru'],1,'LR', 'C');
+     $pdf->Ln();
+
+     // Trait de terminaison
+     $pdf->Cell(array_sum($w),0,'','T');
+
+     $pdf-> Ln();
+
+     $pdf ->SetLineWidth(0.7);
+     $pdf ->Line(0, 145, 220, 145);
+
+     $pdf->SetFont('Arial','',13);
+     $pdf -> cell(40, 40, 'Voici un recapitulatif de votre inscription:',0, 0, 2);
+
+     $pdf-> Ln();
+
+     $pdf -> SetY(-26);
+     $pdf ->SetLineWidth(0.1);
+     $pdf -> SetTextColor(255,0,0);
+     $pdf -> SetDrawColor(255,0,0);
+     $pdf->SetFont('Courier','BI',10);
+
+
+
+     ob_end_clean();
+     $pdf->Output();
+
+
+
 }
-    function entete_table($position_entete) {
-        global $pdf;
-        $pdf->SetDrawColor(183); // Couleur du fond RVB
-        $pdf->SetFillColor(221); // Couleur des filets RVB
-        $pdf->SetTextColor(0); // Couleur du texte noir
-        $pdf->SetY($position_entete);
-        // position de colonne 1 (10mm à gauche)	
-        $pdf->SetX(10);
-        $pdf->Cell(60,8,'Ville',1,0,'C',1);	// 60 >largeur colonne, 8 >hauteur colonne
-        // position de la colonne 2 (70 = 10+60)
-        $pdf->SetX(70); 
-        $pdf->Cell(60,8,'Pays',1,0,'C',1);
-        // position de la colonne 3 (130 = 70+60)
-        $pdf->SetX(130); 
-        $pdf->Cell(30,8,'Repas',1,0,'C',1);
-    
-        $pdf->Ln(); // Retour à la ligne
-    }
-    // AFFICHAGE EN-TÊTE DU TABLEAU
-    // Position ordonnée de l'entête en valeur absolue par rapport au sommet de la page (60 mm)
-    $position_entete = 70;
-    // police des caractères
-    $pdf->SetFont('Helvetica','',9);
-    $pdf->SetTextColor(0);
-    // on affiche les en-têtes du tableau
-    entete_table($position_entete);
     
     
-    $position_detail = 78; // Position ordonnée = $position_entete+hauteur de la cellule d'en-tête (60+8)
-   
-        // position abcisse de la colonne 1 (10mm du bord)
-        $pdf->SetY($position_detail);
-        $pdf->SetX(10);
-        $pdf->MultiCell(60,8,utf8_decode($tab['nomAd']),1,'C');
-        // position abcisse de la colonne 2 (70 = 10 + 60)	
-        $pdf->SetY($position_detail);
-        $pdf->SetX(70); 
-        $pdf->MultiCell(60,8,utf8_decode($tab['prenomAd']),1,'C');
-        // position abcisse de la colonne 3 (130 = 70+ 60)
-        $pdf->SetY($position_detail);
-        $pdf->SetX(130); 
-        $pdf->MultiCell(30,8,$tab['date'],1,'C');
-    
-        // on incrémente la position ordonnée de la ligne suivante (+8mm = hauteur des cellules)	
-        $position_detail += 8; 
   
 // ______________
 
 
 
 // Enfin, le document est terminé et envoyé au navigateur grâce à Output().
-
-ob_end_clean();
-$pdf->Output();
-
-
-
-
-
-
-
 
 // END 
 ?>
